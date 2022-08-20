5a061ebe02ecb197d71babc6ecb44581eabd107b
Edix505 <101802740+Edix505@users.noreply.github.com>
2022-08-20 21:57:05 +0200

    1

---
+++ 1-insertion_sort_list.c
@@ -0,0 +1,36 @@
+#include "sort.h"
+
+/**
+ * insertion_sort_list - sorts a doubly linked list 
+ * order using the Insertion sort algorithm
+ * @list: Double pointer to the head of the linked list
+ *
+ * Return: void
+ */
+void insertion_sort_list(listint_t **list)
+{
+	listint_t *swap_nde, *nxt_swap;
+
+	if (list == NULL || *list == NULL)
+		return;
+	swap_nde = (*list)->nxt;
+	while (swap_nde != NULL)
+	{
+		nxt_swap = swap_nde->nxt;
+		while (swap_nde->prv != NULL && swap_nde->prv->n > swap_nde->n)
+		{
+			swap_nde->prv->nxt = swap_nde->nxt;
+			if (swap_nde->nxt != NULL)
+				swap_nde->nxt->prv = swap_nde->prv;
+			swap_nde->nxt = swap_nde->prv;
+			swap_nde->prv = swap_nde->nxt->prv;
+			swap_nde->nxt->prv = swap_nde;
+			if (swap_nde->prv == NULL)
+				*list = swap_nde;
+			else
+				swap_nde->prv->nxt = swap_nde;
+			print_list(*list);
+		}
+		swap_nde = nxt_swap;
+	}
+}
